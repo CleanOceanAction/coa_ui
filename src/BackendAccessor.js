26f42df2fa39d50ed910d7c3caca020d037fdc45
Matthew Williamson <matthewwilliamson93@gmail.com>
2021-01-09 16:36:09 -0500

    Switch content encoding for cors

--- src/BackendAccessor.js
+++ src/BackendAccessor.js
@@ -7,3 +7,17 @@ export function getData(endpoint) {
     return fetch(`${url}/${endpoint}`,
                 {"method": "GET", "mode": "cors"});
 }
+
+export function postData(endpoint, data) {
+    const url = process.env.NODE_ENV === "development" ?
+        "http://localhost:5000" : "http://coa-flask-app-prod.us-east-1.elasticbeanstalk.com";
+
+    const requestOptions = {
+        "method": "POST",
+        "mode": "cors",
+        "headers": { "Content-Type": "text/plain" },
+        "body": JSON.stringify(data),
+    }
+    console.log(`postData url=${url}/${endpoint}`, requestOptions);
+    return fetch(`${url}/${endpoint}`, requestOptions);
+}
