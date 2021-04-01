be690bcbc30f026d4970ec81ce7aa78c7c47e1df
Jerry Lin <jerrylin1218@gmail.com>
2021-04-01 18:20:00 -0400

    make button insensitive until all fields filled out

--- src/contributions/UserSignInForm.js
+++ src/contributions/UserSignInForm.js
@@ -1,6 +1,6 @@
 import './UserSignInForm.css';
 
-import React, { useState, useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { PopupWarning } from "../components/Popup";
 import { postData } from "../BackendAccessor.js";
 import { userContext } from "./UserContext";
@@ -10,14 +10,10 @@ export default function UserSignInForm(props) {
     const [name, setName] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [submitDisabled, setSubmitDisabled] = useState("");
     const [warning, setWarning] = useState("");
 
     const attemptLogIn = () => {
-        if (!username || !password || !name) {
-            setWarning("Please enter all log-in information.");
-            return;
-        }
-
         postData('login', {"username": username, "password": password})
             .then((response) => {
                 response.json().then((data) => {
@@ -25,6 +21,8 @@ export default function UserSignInForm(props) {
                         "name": name,
                         "token": data.token
                     });
+                }).catch(() => {
+                    setWarning("Login failed - please check your username and password.")
                 });
             });
     };
@@ -34,39 +32,43 @@ export default function UserSignInForm(props) {
         attemptLogIn();
     };
 
+    useEffect(() => {
+        setSubmitDisabled((name && username && password) ? "" : "disabled");
+    }, [name, username, password]);
+
     return(
         <div>
-        <form>
-            <input
-                name="name"
-                type="text"
-                value={name}
-                placeholder="Please enter your name"
-                onChange={(e) => setName(e.target.value)}
+            <form>
+                <input
+                    name="name"
+                    type="text"
+                    value={name}
+                    placeholder="Please enter your name"
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <br/>
+                <input
+                    name="username"
+                    type="text"
+                    value={username}
+                    placeholder="Please enter your username"
+                    onChange={(e) => setUsername(e.target.value)}
+                /><br/>
+                <input
+                    name="password"
+                    type="password"
+                    value={password}
+                    placeholder="Please enter your password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <br/><br/>
+                <input type="submit" value="Enter" disabled={submitDisabled} onClick={onSubmit}/>
+            </form>
+            <PopupWarning
+                show={!!warning}
+                warning={warning}
+                onHide={() => {setWarning("");}}
             />
-            <br/>
-            <input
-                name="username"
-                type="text"
-                value={username}
-                placeholder="Please enter your username"
-                onChange={(e) => setUsername(e.target.value)}
-            /><br/>
-            <input
-                name="password"
-                type="password"
-                value={password}
-                placeholder="Please enter your password"
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <br/><br/>
-            <input type="submit" value="Enter" onClick={onSubmit}/>
-        </form>
-        <PopupWarning
-            show={!!warning}
-            warning={warning}
-            onHide={() => {setWarning("");}}
-        />
         </div>
     );
 }
