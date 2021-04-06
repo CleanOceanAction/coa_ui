3d023365ed16e45cfc1dc58016afd6f84b9b53a3
Matthew Williamson <mwilliamso58@bloomberg.net>
2021-04-06 17:43:34 -0400

    Open up navigation links in a new tab

--- src/Routes.js
+++ src/Routes.js
@@ -3,18 +3,22 @@ import { Route, Switch } from "react-router-dom";
 
 import Contributions from "./contributions/Contributions";
 
+function newTab(url) {
+    window.open(url, "_blank");
+}
+
 export default () =>
   <Switch>
     <Route
         path="/visualizations"
         component={() =>
-            window.location = "http://coa-superset-prod.eba-b9cczgua.us-east-1.elasticbeanstalk.com/superset/dashboard/coa-ui/?preselect_filters=%7B%221%22%3A%20%7B%22__time_range%22%3A%20%22No%20filter%22%7D%7D"
+            newTab("http://coa-superset-prod.eba-b9cczgua.us-east-1.elasticbeanstalk.com/superset/dashboard/coa-ui/?preselect_filters=%7B%221%22%3A%20%7B%22__time_range%22%3A%20%22No%20filter%22%7D%7D")
         }
     />
     <Route
         path="/about"
         component={() =>
-            window.location = "http://www.cleanoceanaction.org/index.php?id=2"
+            newTab("http://www.cleanoceanaction.org/index.php?id=2")
         }
     />
     <Route path="/contributions" exact component={Contributions} />
